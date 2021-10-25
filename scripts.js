c918d0ca9b27af95788a19d32839dfed7738b9e7
Shavaughn Morris <shavaughnmorris@yahoo.com>
2021-10-24 22:31:14 -0500

    Add images, edited and add js and css file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+
+window.onload = (event) => {
+  	let submit = document.querySelector("form button");
+
+    submit.addEventListener("click", e =>{
+        e.preventDefault();
+        let email = document.getElementById("email").value;
+        let msg = document.getElementsByClassName("message")[0];
+        if (email == "") {
+            
+            msg.innerText = "Please enter your email address.";     
+        }
+        else{
+        	console.log(email);
+            msg.innerText = "Thank you! Your email address " + email+ " has been added to our mailing list!";     
+
+        }
+
+    });
+
+
+
+};
